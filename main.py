d0d6b6c37d605baa28864634fc57ed073a091f82
DevSmartFitPeru <52139989+LuisAzaneroCruz@users.noreply.github.com>
2022-12-30 11:28:59 -0500

    ADD_GET_PRODUCTS
    * Se adiciono el método para obtener los productos registrados en OIC_DB Producción.

--- main.py
+++ main.py
@@ -10,12 +10,33 @@ app.config['MYSQL_DB'] = 'oic_db'
 
 mysql = MySQL(app)
 
-@app.route('/planes', methods=['GET'])
+@app.route('/products', methods=['GET'])
 def index():
     cur = mysql.connection.cursor()
-    cur.execute("select *  from oic_db.plan_from_to_version where country ='Peru'")
+    cur.execute("select * from oic_db.product_from_to_version where country ='Peru'")
     rv = cur.fetchall()
-    return jsonify(rv)
+    result = []
+    for row in rv:
+        content = {
+            'id': row[0],
+            'created_at': row[1],
+            'country': row[2],
+            'product_from_to_origin_system': row[3],
+            'product_from_to_operation': row[4],
+            'product_from_to_front_product_id': row[5],
+            'front_product_name': row[6],
+            'erp_item_ar_id': row[7],
+            'erp_item_ar_name': row[8],
+            'erp_item_ar_overdue_recovery_id': row[9],
+            'erp_item_ar_overdue_recovery_name': row[10],
+            'erp_item_ar_discount_id': row[11],
+            'erp_gl_segment_id': row[12],
+            'erp_gl_segment_name': row[13],
+            'erp_ncm_code': row[14],
+            'to_generate_fiscal_document': row[15]
+        }
+        result.append(content)
+        return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+            app.run(debug=True)
