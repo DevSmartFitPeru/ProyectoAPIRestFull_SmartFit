b3716b4c58ba6d36ac2fae95badb2debd91e130f
DevSmartFitPeru <52139989+LuisAzaneroCruz@users.noreply.github.com>
2022-12-30 08:09:29 -0500

    ADD_Test_Conection

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_mysqldb import MySQL
+app = Flask(__name__)
+app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
+app.config['MYSQL_DATABASE_PORT'] = '3306'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB']	= 'dwh'
+app.config['MYSQL_DATABASE_CHARSET'] = 'utf-8'
+mysql = MySQL(app)
 
-name = "Luis"
+@app.route('/user')
+def getAllUser():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT id, firstname, lastname, email, reg_date FROM myguests')
+    data = cur.fetchall()
+    result=[]
+    for row in data:
+        content ={
+        'id':row[0],
+        'firstname': row[1],
+        'lastname': row[2],
+        'email': row[3],
+        'reg_date': row[4]
+          }
+    result.append(content)
+    return jsonify(result)
 
-print(name)
+@app.route('/')
+def index():
+    resultrado ={
+        "name":"Luis",
+        "last_name": "Azañero"
+
+    }
+
+if __name__ == '__main__':
+ app.run(None, 3000, True)
