fb3176c08262cc23675f40572bff60c065861085
DevSmartFitPeru <52139989+LuisAzaneroCruz@users.noreply.github.com>
2022-12-30 11:02:53 -0500

    ADD_GET_PLANES
    * Se adiciono los planes e Perú en el servicio. REST Full.

--- main.py
+++ main.py
@@ -1,38 +1,21 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, request,jsonify
 from flask_mysqldb import MySQL
+
 app = Flask(__name__)
-app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
-app.config['MYSQL_DATABASE_PORT'] = '3306'
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-app.config['MYSQL_DATABASE_DB']	= 'dwh'
-app.config['MYSQL_DATABASE_CHARSET'] = 'utf-8'
+
+app.config['MYSQL_HOST'] = 'oic-db-test.cgzshounia8v.us-east-1.rds.amazonaws.com'
+app.config['MYSQL_USER'] = 'luis.azanero'
+app.config['MYSQL_PASSWORD'] = 'smart#123'
+app.config['MYSQL_DB'] = 'oic_db'
+
 mysql = MySQL(app)
 
-@app.route('/user')
-def getAllUser():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT id, firstname, lastname, email, reg_date FROM myguests')
-    data = cur.fetchall()
-    result=[]
-    for row in data:
-        content ={
-        'id':row[0],
-        'firstname': row[1],
-        'lastname': row[2],
-        'email': row[3],
-        'reg_date': row[4]
-          }
-    result.append(content)
-    return jsonify(result)
-
-@app.route('/')
+@app.route('/planes', methods=['GET'])
 def index():
-    resultrado ={
-        "name":"Luis",
-        "last_name": "Azañero"
-
-    }
+    cur = mysql.connection.cursor()
+    cur.execute("select *  from oic_db.plan_from_to_version where country ='Peru'")
+    rv = cur.fetchall()
+    return jsonify(rv)
 
 if __name__ == '__main__':
- app.run(None, 3000, True)
+    app.run(debug=True)
