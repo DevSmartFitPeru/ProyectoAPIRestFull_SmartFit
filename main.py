8ab734a8b9e06f060537e8e84800812b826f6e32
Daygor <Daygor.pipa@smartfit.com>
2023-08-09 23:12:22 -0500

    UPDATE API KPI PROMOCIONES
    UPDATE API KPI PROMOCIONES

--- main.py
+++ main.py
@@ -1021,7 +1021,7 @@ def kpis(fecha_inicio,fecha_fin):
 def kpipromociones(fecha_inicio,fecha_fin):
     try:
         cursor = connect(aws_access_key_id="AKIA4LTBLLTUCHTCM2ZY", aws_secret_access_key="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", s3_staging_dir="s3://7-smartfit-da-de-lake-artifacts-athena-latam/", region_name="us-east-1", work_group="peru", schema_name="prod_lake_modeled_refined").cursor()
-        cursor.execute(" select prom.id id_promocion ,date_format(prom.starts_at,'%Y-%m-%d')  fecha_inicio_promo ,date_format(prom.expires_at,'%Y-%m-%d') vigencia_promo ,prom.active estado_promocion ,replace(prom.title, '|',' ')title ,pur.id id_compra ,date_format(pur.created_at,'%Y-%m-%d') fecha_de_compra ,date_format(pur.confirmed_at,'%Y-%m-%d')  fecha_de_confirm_compra ,date_format(pur.cancelled,'%Y-%m-%d')  fecha_cancel_compra ,date_format(pur.expired_at,'%Y-%m-%d') fecha_exp_compra ,pur.person_id cod_matricula ,cli.cliente_nome nombre_cliente ,cli.cliente_idade  edad_cliente ,peo.status_aluno ,peo.status_catraca ,plan.name nombre_plan ,loc.acronym cod_unidad_compra ,case 	when pur.created_at is not null and pur.expired_at is null then 'Activo' 	else 'Cancelado' end Status_compra from prod_lake_ss_refined.purchases pur left join prod_lake_modeled_refined.dim_promotions prom on pur.promotion_id  = prom.id left join prod_lake_modeled_refined.dim_plans plan on pur.plan_id = plan.id left join prod_lake_modeled_refined.dim_locations loc on pur.original_location_id = loc.id left join prod_lake_modeled_refined.dim_people peo on pur.person_id  = peo.person_id left join prod_lake_modeled_salesforce_latam.salesforce_dim_clientes_latam cli on cast(pur.person_id  as varchar(50)) = cli.cliente_person_id where pur.promotion_id  in (11832, 11983, 12198, 12401, 12448, 12543, 12688, 13071, 13127, 13450, 13600, 13565) and date_format(pur.created_at,'%Y-%m-%d') between '"+str(fecha_inicio)+"' and '"+str(fecha_fin)+"' ")
+        cursor.execute(" select prom.id id_promocion ,date_format(prom.starts_at,'%Y-%m-%d') fecha_inicio_promo ,date_format(prom.expires_at,'%Y-%m-%d') vigencia_promo ,prom.active estado_promocion ,replace(prom.title, '|', ' ')title ,pur.id id_compra ,date_format(pur.created_at,'%Y-%m-%d') fecha_de_compra ,date_format(pur.confirmed_at,'%Y-%m-%d') fecha_de_confirm_compra ,date_format(pur.cancelled,'%Y-%m-%d') fecha_cancel_compra ,date_format(pur.expired_at,'%Y-%m-%d') fecha_exp_compra ,pur.person_id cod_matricula ,cli.cliente_nome nombre_cliente ,cli.cliente_idade edad_cliente ,peo.status_aluno ,peo.status_catraca ,plan.name nombre_plan ,loc.acronym cod_unidad_compra ,case 	when pur.created_at is not null and pur.expired_at is null then 'Activo' 	else 'Cancelado' end status_compra ,wel.payment_id ,date_format(wel.due_at,'%Y-%m-%d')due_at ,date_format(wel.payed_at,'%Y-%m-%d')payed_at ,wel.amount_paid ,wel.payment_state from 	prod_lake_ss_refined.purchases pur left join prod_lake_modeled_refined.welshers wel on pur.id = wel.purchase_id and wel.payment_state = 'payed' left join prod_lake_modeled_refined.dim_promotions prom on pur.promotion_id = prom.id left join prod_lake_modeled_refined.dim_plans plan on pur.plan_id = plan.id left join prod_lake_modeled_refined.dim_locations loc on pur.original_location_id = loc.id left join prod_lake_modeled_refined.dim_people peo on pur.person_id = peo.person_id left join prod_lake_modeled_salesforce_latam.salesforce_dim_clientes_latam cli on cast(pur.person_id as varchar(50)) = cli.cliente_person_id where pur.promotion_id in (11832, 11983, 12198, 12401, 12448, 12543, 12688, 13071, 13127, 13450, 13600, 13565) and date_format(pur.created_at,'%Y-%m-%d') between '"+str(fecha_inicio)+"' and '"+str(fecha_fin)+"' ")
         resultado = []
         for row in cursor:
             content = {
@@ -1041,7 +1041,13 @@ def kpipromociones(fecha_inicio,fecha_fin):
                         'status_aluno': row[13],
                         'status_catraca': row[14],
                         'nombre_plan': row[15],
-                        'cod_unidad_compra': row[16]
+                        'cod_unidad_compra': row[16],
+                        'status_compra': row[17],
+                        'payment_id': row[18],
+                        'due_at': row[19],
+                        'payed_at': row[20],
+                        'amount_paid': row[21],
+                        'payment_state': row[22]
                      }
             resultado.append(content)
         return jsonify(resultado)
