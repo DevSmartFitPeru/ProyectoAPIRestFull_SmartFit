23d5ba8b345bdb48ea3bcf45a5f6a38b81464da5
DevSmartFitPeru <52139989+LuisAzaneroCruz@users.noreply.github.com>
2024-01-15 07:32:35 -0500

    Return_latam

--- main.py
+++ main.py
@@ -1318,7 +1318,7 @@ def oracle_sovos(fecha_inicio,fecha_fin):
 def invoice_ss_latam(fecha_inicio,fecha_fin):
     try:
         cursor = connect(aws_access_key_id="AKIA4LTBLLTUCHTCM2ZY", aws_secret_access_key="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", s3_staging_dir="s3://7-smartfit-da-de-lake-artifacts-athena-latam/", region_name="us-east-1", work_group="peru", schema_name="prod_lake_modeled_refined").cursor()
-        cursor.execute("select id_payment,status_pagamento,date_format(payed_at , '%Y-%m-%d') payed_at ,amount_paid ,CASE WHEN forma_pagamento is null THEN 'Forma de Pago NO Identificada' ELSE forma_pagamento end as forma_pagamento ,country ,acronym,CASE WHEN minifactu_id is null THEN 0 ELSE minifactu_id end minifactu_id , CASE WHEN error is null THEN 'Transacción Sin Errores' ELSE error end error from prod_lake_modeled_refined.minifactu_otc where date_format(payed_at, '%Y-%m-%d') BETWEEN '"+str(fecha_inicio)+"' and '"+str(fecha_fin)+"' and country ='Colômbia'")
+        cursor.execute("select id_payment,status_pagamento,date_format(payed_at , '%Y-%m-%d') payed_at ,amount_paid ,CASE WHEN forma_pagamento is null THEN 'Forma de Pago NO Identificada' ELSE forma_pagamento end as forma_pagamento ,country ,acronym,CASE WHEN minifactu_id is null THEN 0 ELSE minifactu_id end minifactu_id , CASE WHEN error is null THEN 'Transacción Sin Errores' ELSE error end error from prod_lake_modeled_refined.minifactu_otc where date_format(payed_at, '%Y-%m-%d') BETWEEN '"+str(fecha_inicio)+"' and '"+str(fecha_fin)+"' and country  in('Peru','Colômbia','México','Chile')")
         records = cursor.fetchall()
         for row in records:
             id_payment = str(row[0])
