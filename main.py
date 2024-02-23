a7531f12fe016e4e30b9cb39bb492fe738fd5375
Michael Balladares <145366695+MichaelBalladaresSmarFit@users.noreply.github.com>
2024-02-23 11:29:09 -0500

    INSERTAR REGISTRO DE FORMA INDIVIDUAL Y NO GRUPAL O BLOQUE

--- main.py
+++ main.py
@@ -878,7 +878,7 @@ def pagos_procesados_aws(fecha_inicio,fecha_fin):
             query_sql_insert = 'insert into "ATHENA"."PAGOS_PROCESADOS_SMARTSYSTEM_LATAM"  (ID_PAYMENT,STATUS_PAGAMENTO,PAYET_AT,AMOUNT_PAID,FORMA_PAGAMENTO,COUNTRY,ACRONYM,MINIFACTU_ID,ERROR) ' \
                               " values("'' + id_payment + ''","'' + "'" + str(status_pagamento) + "'" + ''","'' + "'" + str(payed_at) + "'" + ''","'' + amount_paid + ''","'' + "'" + str(forma_pagamento) + "'" + ''","'' + "'" + str(country) + "'" + ''","'' + "'" + str( acronym) + "'" + ''","'' + minifactu_id + ''","'' + "'" + str(error) + "'" + ''") "
             cur.execute(query_sql_insert)
-        connposgresql.commit()
+            connposgresql.commit()
         return 'Registro exitoso'
     except Exception as e:
         print(e)
